ea6de0f3c42f7a061567de02d2ed1b16ef2ccc8c
Bogdan <boggzon@gmail.com>
2024-02-09 00:40:22 +0300

    working to roman convertion in range of 3999 integer

--- main.cpp
+++ main.cpp
@@ -37,40 +37,37 @@ class RomanHelper{
       splitNumber(n, parts);
       std::cout << "Разбиение числа на части" << std::endl;
       for (int c = 3; c>= 0; c--) {
-        std::cout << parts[c] << " ";
-      }
+        for (int i = l - 1; i >= 0; i--) {
+          int flag = 0;
+          std::cout << "i arab is " << num[i].arab << std::endl;
+          std::cout << "parts is " << parts[c] << std::endl;
+          std::cout << parts[c] << " = " << num[i+1].arab << " - " << num[i-1].arab << std::endl;
 
-      for (int i = l - 1; i >= 0; i--) {
-        int flag = 0;
-        std::cout << "i arab is " << num[i].arab << std::endl;
-        std::cout << "n is " << n << std::endl;
-        std::cout << n << " = " << num[i+1].arab << " - " << num[i-1].arab << std::endl;
-
-        if (n == num[i+1].arab - num[i-1].arab){
-          n -= num[i+1].arab - num[i-1].arab;
-          answer += num[i-1].roman + num[i+1].roman;
-          //std::cout << "in if cycle " << n << " = " << num[i+1].arab << " - " << num[i-1].arab << std::endl;
-          std::cout << "if answer now is " << answer << std::endl;
-        }
-        else if (n == num[i+1].arab - num[i].arab) {
-          n -= num[i+1].arab - num[i].arab;
-          answer += num[i].roman + num[i+1].roman;
-          std::cout << "in if cycle " << n << " = " << num[i+1].arab << " - " << num[i].arab << std::endl;
-          std::cout << "if answer now is " << answer << std::endl;
-        }
-        else {
-          while (n >= num[i].arab) {
-            //std::cout << "n is " << n << std::endl;
-            //std::cout << "num[i].arab is " << num[i].arab << std::endl;
-            n -= num[i].arab;
-            answer += num[i].roman;
-            flag++;
-            std::cout << "while answer now is " << answer << std::endl;
-            // if (flag > 3) { //очень костыльно, работает только с IIII -> IV 
-            //   answer = answer.erase(1, 3);
-            //   answer += num[i + 1].roman;
-            //   std::cout << "flag answer now is " << answer << std::endl;
-            // }
+          if (parts[c] == num[i+1].arab - num[i-1].arab){
+            parts[c] -= num[i+1].arab - num[i-1].arab;
+            answer += num[i-1].roman + num[i+1].roman;
+            //std::cout << "in if cycle " << n << " = " << num[i+1].arab << " - " << num[i-1].arab << std::endl;
+            std::cout << "if answer now is " << answer << std::endl;
+          }
+          else if (parts[c] == num[i+1].arab - num[i].arab) {
+            parts[c] -= num[i+1].arab - num[i].arab;
+            answer += num[i].roman + num[i+1].roman;
+            std::cout << "in if cycle " << parts[c] << " = " << num[i+1].arab << " - " << num[i].arab << std::endl;
+            std::cout << "if answer now is " << answer << std::endl;
+          }
+          else {
+            while (parts[c] >= num[i].arab) {
+              //std::cout << "n is " << n << std::endl;
+              //std::cout << "num[i].arab is " << num[i].arab << std::endl;
+              parts[c] -= num[i].arab;
+              answer += num[i].roman;
+              flag++;
+              std::cout << "while answer now is " << answer << std::endl;
+              // if (flag > 3) { //очень костыльно, работает только с IIII -> IV 
+              //   answer = answer.erase(1, 3);
+              //   answer += num[i + 1].roman;
+              //   std::cout << "flag answer now is " << answer << std::endl;
+              // }
           }
         }
 
@@ -82,7 +79,9 @@ class RomanHelper{
         // }
 
       //std::cout << "Roman: " << num[i].roman << " Arabic: " << num[i].arab << std::endl;
+        }        
       }
+
       std::cout << "answer is " << answer << std::endl;
       return answer;
     }
