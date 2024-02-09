a3f0b0386c38a0d84a7d9427e713ab37ef26e0ea
Bogdan <boggzon@gmail.com>
2024-02-09 13:45:39 +0300

    добвален тест 8 to_roman

--- tests.cpp
+++ tests.cpp
@@ -7,31 +7,43 @@ int main() {
     //Тест 1
     unsigned int n1 = 1000;
     assert(RomanNumerals.to_roman(n1) == "M");
-
+    std::cout << n1 << " = M" << std::endl;
+    
     //Тест 2
     unsigned int n2 = 4;
     assert(RomanNumerals.to_roman(n2) == "IV");
+    std::cout << n2 << " = IV" << std::endl;
 
     // Тест 3
     unsigned int n3 = 1;
     assert(RomanNumerals.to_roman(n3) == "I");
+    std::cout << n3 << " = I" << std::endl;
 
     // Тест 4
     unsigned int n4 = 9;
     assert(RomanNumerals.to_roman(n4) == "IX");
+    std::cout << n4 << " = IX" << std::endl;
 
     // Тест 5
     unsigned int n5 = 1990;
     assert(RomanNumerals.to_roman(n5) == "MCMXC");
+    std::cout << n5 << " = MCMXC" << std::endl;
 
     // Тест 6
     unsigned int n6 = 2008;
     assert(RomanNumerals.to_roman(n6) == "MMVIII");
+    std::cout << n6 << " = MMVIII" << std::endl;
 
     // Тест 7
     unsigned int n7 = 3999;
     assert(RomanNumerals.to_roman(n7) == "MMMCMXCIX");
-    
+    std::cout << n7 << " = MMMCMXCIX" << std::endl;
+
+    // Тест 8
+    unsigned int n8 = 3560;
+    assert(RomanNumerals.to_roman(n8) == "MMMDLX");
+    std::cout << n8 << " = MMMDLX" << std::endl;
+
     //Тесты from_roman
 
     //Тест 1
